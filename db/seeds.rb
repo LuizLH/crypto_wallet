58e8a436e61abbef6535f2f66aa4f31de3f85ea2
LuizLH <luizlh@gmail.com>
2023-02-18 20:15:22 -0300

    Seção 3: 67-Refatorando o seeds.rb (find_or_create_by)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-puts "Inserindo moedas..."
+spinner = TTY::Spinner.new("[:spinner] Cadastrando Moedas...")
+spinner.auto_spin
 
-Coin.create!(
+coins = [
+   {
     description: "Bitcoin",
     acronym: "BTC",
     url_image: "https://img.freepik.com/vetores-premium/simbolo-de-bitcoin-de-moeda-dourada-de-criptografia-isolado-em-fundo-transparente_372769-976.jpg"
- )
+   },
  
- Coin.create!(
+   {
     description: "Ethereum",
     acronym: "ETH",
     url_image: "https://png.pngtree.com/png-clipart/20210511/ourmid/pngtree-this-is-ethereum-cryptocurrency-vector-illustration-png-image_3279437.jpg"
- )
+   },
  
- Coin.create!(
+   {
     description: "Tether",
     acronym: "USDT",
     url_image: "https://logospng.org/wp-content/uploads/tether.png"
- )
+   }
+]
 
- puts "Moedas criadas com sucesso..."
+ coins.each do |coin|
+   Coin.find_or_create_by!(coin)
+ end
+
+ spinner.success("(Concluído)")
